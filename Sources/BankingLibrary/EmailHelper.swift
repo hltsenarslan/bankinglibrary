1a5e0bbb5439a0044a3c39ecf301948083d61052
Halit ŞENARSLAN <halitsenarslan@gmail.com>
2020-02-03 11:56:07 +0300

    public

--- Sources/BankingLibrary/EmailHelper.swift
+++ Sources/BankingLibrary/EmailHelper.swift
@@ -10,7 +10,7 @@ import Foundation
 public class EmailValidator  {
     public init() { }
     
-    func isValidEmail(email: String) -> Bool {
+    public func isValidEmail(email: String) -> Bool {
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
 
         let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
